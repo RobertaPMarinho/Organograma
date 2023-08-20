a14fa2a2d6eee9ab2190a7bb7272bfcf7430c596
Roberta Pereira Marinho <robertapmarinho@gmail.com>
2023-08-20 16:49:52 -0300

    First commit

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,12 @@
+import './Banner.css'
+
+function Banner() {
+  // JSX - parece HTML, porém NÃO É, ele é JSX!
+  return (
+    <header className="banner">
+    <img src="/imagens/banner.png" alt="Banner principal da página do Organo"/>
+    </header>
+  )
+}
+
+export default Banner
